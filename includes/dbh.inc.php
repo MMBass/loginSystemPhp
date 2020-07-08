6c029932db208b7ab927af4b9e103900583a3779
MMBass <51884406+MMBass@users.noreply.github.com>
2020-07-08 20:20:15 +0300

    update cradentials

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$serverName = "localhost";
-$dbUserName = "root";
-$dbName = "loginsystemphp";
-$dbPassword = "";
+$dbserverName = "lolyz0ok3stvj6f0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbUserName = "l57g6xqcchkh24c7";
+$dbName = "mh5sg71nlsivo7lr";
+$dbPassword = "equzxjw9gg4oes8q";
 
-$conn = mysqli_connect($serverName,$dbUserName,$dbPassword,$dbName);
+$conn = mysqli_connect($dbserverName,$dbUserName,$dbPassword,$dbName);
 
 if(!$conn){
   die("connection faild: ".mysql_connetion_error());
